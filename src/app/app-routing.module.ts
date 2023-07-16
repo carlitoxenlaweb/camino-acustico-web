46f6c7824224d352a7f95e00534fc939cece3c3c
Carlos Martinez <carloscemg@gmail.com>
2023-07-16 01:59:23 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PrivacyComponent } from './terms/privacy/privacy.component';
+import { CookiesComponent } from './terms/cookies/cookies.component';
+import { ConditionsComponent } from './terms/conditions/conditions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'terms/privacy', component: PrivacyComponent },
+  { path: 'terms/cookies', component: CookiesComponent },
+  { path: 'terms/conditions', component: ConditionsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrivacyComponent } from './terms/privacy/privacy.component';
+import { CookiesComponent } from './terms/cookies/cookies.component';
+import { HomeComponent } from './home/home.component';
+import { ConditionsComponent } from './terms/conditions/conditions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrivacyComponent,
+    CookiesComponent,
+    HomeComponent,
+    ConditionsComponent
   ],
   imports: [
     BrowserModule,
